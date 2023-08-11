8e46370015835a0b60a717cb8840b98cfd859c7e
Sakura-gkf <94181508+Sakura-gkf@users.noreply.github.com>
2023-08-11 07:44:13 +0000

    newconfig Updated By Github Actions

--- js/imgloaded.js
+++ js/imgloaded.js
@@ -67,8 +67,8 @@ const executeLoad = (config, target) => {
 };
 
 const config = {
-    smallSrc: '/img/toutuxiao.jpg', // 小图链接 尽可能配置小于100k的图片
-    largeSrc: '/img/toutuda.jpg', // 大图链接 最终显示的图片
+    smallSrc: '/img/topimgsm.jpg', // 小图链接 尽可能配置小于100k的图片
+    largeSrc: '/img/topimgbg.png', // 大图链接 最终显示的图片
     mobileSmallSrc: '/img/mbtopsm.webp', // 手机端小图链接 尽可能配置小于100k的图片
     mobileLargeSrc: '/img/mbtopla.webp', // 手机端大图链接 最终显示的图片
     enableRoutes: ['/'],
